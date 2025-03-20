aaaff44393cc8aa5b511d73abb1b76ad3e0bdf49
Zaid 10p <zaid.ibrahim@10pearls.com>
2025-03-20 12:56:40 +0500

    initial commit for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './userdata';
-import { TasksComponent } from './tasks/tasks.component';
-import { User } from '../types';
-// import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUser?: User;
-
-  onSelectedUser(user: any) {
-    this.selectedUser = user;
-  }
-}
+export class AppComponent {}
