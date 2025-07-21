6839e22da8478b63c7a7041f039d37a77ac1475a
Zaid 10p <zaid.ibrahim@10pearls.com>
2025-07-21 17:42:48 +0500

    reactive form simple example signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+const mustcontianNumber = (control: AbstractControl) => {
+  const hasNumber = /\d/.test(control.value || '');
+  return hasNumber ? null : { mustContainNumber: true };
+};
+export const passwordsMatchValidator = (form: AbstractControl) => {
+  const password = form.get('password')?.value;
+  const confirmPassword = form.get('confirmPassword')?.value;
+
+  if (password !== confirmPassword) {
+    return { passwordsMismatch: true };
+  }
+
+  return null;
+};
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  form = new FormGroup(
+    {
+      email: new FormControl('', {
+        validators: [Validators.email, Validators.required],
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          mustcontianNumber,
+        ],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          mustcontianNumber,
+        ],
+      }),
+    },
+    {
+      validators: passwordsMatchValidator, // 👈 Attach group-level validator here
+    }
+  );
+  destroy = inject(DestroyRef);
+
+  ngOnInit() {
+    const data = window.localStorage.getItem('formData');
+    if (data) {
+      const parsedData = JSON.parse(data);
+      if (parsedData.email) {
+        this.form.controls.email.setValue(parsedData.email);
+      }
+    }
+    const sub = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        window.localStorage.setItem(
+          'formData',
+          JSON.stringify({ email: value.email })
+        );
+      });
+
+    this.destroy.onDestroy(() => {
+      sub.unsubscribe();
+    });
+  }
+  getEmailInValid() {
+    return this.form.controls.email.invalid && this.form.controls.email.touched;
+  }
+  getPasswordInValid() {
+    return (
+      this.form.controls.password.invalid && this.form.controls.password.touched
+    );
+  }
+  getPasswordsMatch() {
+    return (
+      this.form.controls.confirmPassword.touched &&
+      !!this.form.errors?.['passwordsMismatch']
+    );
+  }
+
+  onSubmit() {
+    console.log('FORM ', this.form.status);
+    if (this.form.invalid) {
+      console.error('Form is invalid');
+      return;
+    }
+    console.log('Form submitted successfully:', this.form.value);
+    // Here you would typically handle the form submission,
+  }
+}
